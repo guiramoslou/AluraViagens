02c973746b55a9090db86164d579d8e82028db99
Guilherme Ramos Lourenco <guiramoslou@gmail.com>
2021-03-18 12:48:31 -0300

    Recuperada TableView

---
+++ Alura Viagens/Home/Controller/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Alura Viagens
+//
+//  Created by Guilherme Ramos Lourenco on 12/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    
+    @IBOutlet weak var tabelaViagens: UITableView!
+    @IBOutlet weak var hoteisView: UIView!
+    @IBOutlet weak var pacotesView: UIView!
+    
+    let listaDeViagens: Array<Viagem> = ViagemDAO().retornaTodasAsViagens()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tabelaViagens.dataSource = self
+        self.tabelaViagens.delegate = self
+        self.hoteisView.layer.cornerRadius = 10
+        self.pacotesView.layer.cornerRadius = 10
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listaDeViagens.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        
+        let viagemAtual = listaDeViagens[indexPath.row]
+
+        cell.labelTitulo.text = viagemAtual.titulo
+        cell.labelQuantidadeDeDias.text = "\(viagemAtual.quantidadeDeDias) dias"
+        cell.labelPreco.text = viagemAtual.preco
+        cell.imagemViagem.image = UIImage(named: viagemAtual.caminhoDaImagem)
+        cell.imagemViagem.layer.cornerRadius = 10
+        cell.imagemViagem.layer.masksToBounds = true
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 175
+    }
+
+
+}
+
---
+++ Alura Viagens/Home/View/TableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  TableViewCell.swift
+//  Alura Viagens
+//
+//  Created by Guilherme Ramos Lourenco on 17/03/21.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    @IBOutlet weak var labelTitulo: UILabel!
+    @IBOutlet weak var labelQuantidadeDeDias: UILabel!
+    @IBOutlet weak var labelPreco: UILabel!
+    @IBOutlet weak var imagemViagem: UIImageView!
+}
